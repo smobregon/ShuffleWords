5f8ddc2341f627bfbc267845c0115ec52b40f0f3
Savannah <smobregon@mail.lipscomb.edu>
2018-07-11 19:56:48 -0500

    Add files via upload

---
+++ Shuffle.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Shuffle
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first word: ");
+		String firstWord = sc.nextLine().toUpperCase();
+
+		System.out.print("Enter the second word: ");
+		String secondWord = sc.nextLine().toUpperCase();
+
+		//System.out.println(firstWord.charAt(0));
+		//int length = firstWord.length();
+
+		if (firstWord.length() == secondWord.length()) {
+			System.out.print("The shuffle output is ");
+
+			for (int i = 0, length = firstWord.length(); i < length; i++) {
+				System.out.print(firstWord.charAt(i));
+				System.out.print(secondWord.charAt(i));
+			}	
+			System.out.println();		
+
+		} else {
+
+			System.out.println("Invalid inputs");
+
+		}
+
+	}
+
+}
