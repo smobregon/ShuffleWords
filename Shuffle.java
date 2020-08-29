5062d2d0762ef477525e4d7aec9dbbccc5726818
Savannah Obregon <savannah.obregon@lifeway.com>
2020-08-29 12:27:45 -0400

    restart program if given invalid inputs

--- Shuffle.java
+++ Shuffle.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class Shuffle
-{
-	public static void main(String[] args) 
-	{
+public class Shuffle {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the first word: ");
 		String firstWord = sc.nextLine().toUpperCase();
@@ -11,21 +9,19 @@ public class Shuffle
 		System.out.print("Enter the second word: ");
 		String secondWord = sc.nextLine().toUpperCase();
 
-		//System.out.println(firstWord.charAt(0));
-		//int length = firstWord.length();
-
 		if (firstWord.length() == secondWord.length()) {
 			System.out.print("The shuffle output is ");
 
 			for (int i = 0, length = firstWord.length(); i < length; i++) {
 				System.out.print(firstWord.charAt(i));
 				System.out.print(secondWord.charAt(i));
-			}	
-			System.out.println();		
+			}
+			System.out.println();
 
 		} else {
 
-			System.out.println("Invalid inputs");
+			System.out.println("Invalid inputs, please enter two words of the same length\n");
+			main(new String[0]);
 
 		}
 
